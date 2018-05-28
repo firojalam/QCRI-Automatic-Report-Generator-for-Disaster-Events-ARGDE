05ede6bc34f62d77111a2fe80b220fc04ec8988c
adhithyaarun <adhithya.arun@students.iiit.ac.in>
2018-05-28 17:55:37 +0300

    Added controller code to retrieve label-wise and sentiment-wise data

--- ARGDE/api/controllers/DataController.js
+++ ARGDE/api/controllers/DataController.js
@@ -50,4 +50,45 @@ module.exports = {
 				}
 			});
 	},
+	retrieveLabel: async function(req, res){
+			await Label_frequency.find({
+				where: { code: req.param('collection'), },
+				sort: { date: 1, hour: 1, minute: 1,},
+			}).exec(function(err, records){
+				if(err)
+				{
+					sails.log.error("Error name: "+err.name+"	"+"Error code: "+err.code);
+					return res.serverError(err);
+				}
+				else
+				{
+					sails.log.info("Label data retrieved, passing to view");
+					return res.view('Dashboard/label_freq', {label_data: records});
+				}
+			});
+	},
+	retrieveSentiment: async function(req, res){
+			await Label_frequency.find({
+				where: { code: req.param('collection'), },
+				sort: { date: 1, hour: 1, minute: 1,},
+				or: [
+					{ Label_frequency.attributes.class_label.columnName: 'Very Positive' },
+					{ Label_frequency.attributes.class_label.columnName: 'Positive' },
+					{ Label_frequency.attributes.class_label.columnName: 'Neutral' },
+					{ Label_frequency.attributes.class_label.columnName: 'Negative' },
+					{ Label_frequency.attributes.class_label.columnName: 'Very Negative' },
+				],
+			}).exec(function(err, records){
+				if(err)
+				{
+					sails.log.error("Error name: "+err.name+"	"+"Error code: "+err.code);
+					return res.serverError(err);
+				}
+				else
+				{
+					sails.log.info("Sentiment data retrieved, passing to view");
+					return res.view('Dashboard/sentiment_freq', {sentiment_data: records});
+				}
+			});
+	},
 };
