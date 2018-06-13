431c892fd5c6c02b9bd417609d5ce93808c9c8cb
Abbas Ahmed <aza16@pitt.edu>
2018-06-13 19:25:36 +0300

    Added the Frequency graph,  and set the default reset of the graph to Bar type.

--- ARGDE/assets/js/interface.js
+++ ARGDE/assets/js/interface.js
@@ -19,7 +19,7 @@ var allData = {
 var graphType = {
   class: 'stacked-bar',
   sentiment: 'stacked-bar',
-  frequency: 'stacked-bar',
+  frequency: 'bar',
   relevancy: 'stacked-bar',
   damage: 'stacked-bar',
 };
@@ -55,6 +55,7 @@ function data()
 {
   sockets.minute.get(queries['minute'], function(data, json_obj){
     allData.minute_data = data['minute_data'];
+    generate.frequency(graphRes.frequency);
   });
   sockets.hour.get(queries['hour'], function(data, json_obj){
     allData.hour_data = data['hour_data'];
@@ -97,7 +98,6 @@ generate.class = function(res)
     var fdat = (Number(entry.frequency));
     freqData = freqData.concat(fdat);
     ldat = String(entry.class_label);
-    var class_str  = ldat.toString();
     clData = clData.concat(ldat);
     var mdat = String(entry.minute);
     minData = minData.concat(mdat);
@@ -177,7 +177,13 @@ generate.class = function(res)
       x: 'date',
       y: 'frequency',
       color: 'label',
+      settings:{
+        asyncRendering: true,
+      },
       guide: {
+        x:{
+          label: 'Minute'
+        },
         y:{
           label: 'Frequency',
         },
@@ -227,14 +233,137 @@ generate.class = function(res)
   }
 
   charts.class.renderTo('#ClassChart',{width: chartDimensions.class.width, height: chartDimensions.class.height});
+}
 
-  $(window).resize(function() {
-    clearTimeout(window.resizedFinished);
-    window.resizedFinished = setTimeout(function(){
-        charts.class.resize({width: chartDimensions.class.width, height: chartDimensions.class.height});
-    }, 250);
+generate.frequency = function(res)
+{
+  var freqData = [];
+  var minData = [];
+  var hourData = [];
+  var dateData = [];
+  var monthData = [];
+  var yearData = [];
+  var data = [];
+  console.log(allData.minute_data);
+
+  allData.minute_data.forEach(function(entry){
+    freqData = freqData.concat(Number(entry.frequency));
+    minData = minData.concat(String(entry.minute));
+    hourData = hourData.concat(String(entry.hour).substr(0,2));
+    var ddat = new Date(entry.date);
+    var date = String(ddat.getDate());
+    var year = String(ddat.getFullYear());
+    var month = Number(ddat.getMonth());
+    var newMonth = month + 1;
+    var newMon = String(newMonth);
+    dateData = dateData.concat(date);
+    monthData = monthData.concat(newMon);
+    yearData = yearData.concat(year);
+   });
+
+  for(i in freqData)
+  {
+    var timeStamp;
+    switch(res)
+    {
+      case 'minute':
+            if(minData[i]<10)
+            {
+              timeStamp = ", "+String(hourData[i]) + ":0" + String(minData[i]);
+            }
+            else
+            {
+              timeStamp = ", "+String(hourData[i]) + ":" + String(minData[i]);
+            }
+            break;
+      case 'hour':
+            timeStamp = ", "+String(hourData[i]) + ":00";
+            break;
+
+      case 'day' :
+            timeStamp = "";
+            break;
+
+      default:
+            if(minData[i]<10)
+            {
+              timeStamp = ", "+String(hourData[i]) + ":0" + String(minData[i]);
+            }
+            else
+            {
+              timeStamp = ", "+String(hourData[i]) + ":" + String(minData[i]);
+            }
+            break;
+    }
+    var dateStamp = String(yearData[i])+"-"+String(Number(monthData[i]))+"-"+String(dateData[i]) + timeStamp;
+    var formatFreq = Number(freqData[i]);
+    let temp = {date: dateStamp, frequency: formatFreq};
+    let flag = false;
+    data = data.map(function(datum){
+      if((temp.date == datum.date))
+      {
+        datum.frequency += temp.frequency;
+        flag = true;
+        return datum;
+      }
+      else
+      {
+        return datum;
+      }
+    });
+    if(flag == false)
+    {
+      data.push(temp);
+    }
+  };
+
+  console.log(data);
+
+  charts.frequency = new tauCharts.Chart({
+      data: data,
+      type: graphType['frequency'],
+      x: 'date',
+      y: 'frequency',
+      color: 'frequency',
+      settings:{
+                asyncRendering: true,
+              },
+      guide: {
+        x:{
+          label: 'Time'
+        },
+        y:{
+          label: 'Frequency',
+        }
+      },
+      plugins: [
+          tauCharts.api.plugins.get('tooltip')({fields: ['date','frequency']}),
+          tauCharts.api.plugins.get('exportTo')({
+             cssPaths:['https://cdn.jsdelivr.net/taucharts/latest/tauCharts.min.css']
+          }),
+          tauCharts.api.plugins.get('floating-axes')(),
+      ],
   });
 
+  switch(res)
+  {
+    case 'minute':
+      chartDimensions.frequency.width = 4500;
+      chartDimensions.frequency.height = 600;
+      break;
+
+    case 'hour':
+      chartDimensions.frequency.width = 1550;
+      chartDimensions.frequency.height = 600;
+      break;
+
+    case 'day' :
+      chartDimensions.frequency.width = 1550;
+      chartDimensions.frequency.height = 600;
+      break;
+  }
+
+  charts.frequency.renderTo('#FrequencyChart',{width: chartDimensions.frequency.width, height: chartDimensions.frequency.height});
 }
 
 generate.sentiment = function(res)
@@ -249,7 +378,6 @@ generate.sentiment = function(res)
   let hour;
   let minute;
   let time;
-  let disDay;
   datacopy = allData.sentiment_data.slice();
   datacopy.forEach(function(sentiment)
   {
@@ -258,9 +386,7 @@ generate.sentiment = function(res)
     year = date.getFullYear();
     month = date.getMonth()+1;
     day = date.getDate();
-    disDay = Number(obj.day);
     hour = obj.hour.substr(0,2);
-
     obj.sentiment = obj.class_label;
     switch(res)
     {
@@ -301,25 +427,6 @@ generate.sentiment = function(res)
           break;
 
       case 'day':
-          var flag = false;
-          obj.compiled_time = String(year)+"-"+String(month)+"-"+String(day)+", Day: "+disDay;
-          obj.frequency = Number(obj.frequency);
-          final_data = final_data.map(function(datum){
-            if((obj.compiled_time==datum.compiled_time) && (obj.sentiment==datum.sentiment))
-            {
-              datum.frequency += obj.frequency;
-              flag = true;
-              return datum;
-            }
-            else
-            {
-              return datum;
-            }
-          });
-          if(flag == false)
-          {
-            final_data.push(obj);
-          }
           break;
 
       default:
@@ -333,6 +440,9 @@ generate.sentiment = function(res)
               x: 'compiled_time',
               y: 'frequency',
               color: 'sentiment',
+              settings:{
+                asyncRendering: true,
+              },
               guide:{
                 x:{label: 'Minute'},
                 y:{label: 'Frequency'},
@@ -368,25 +478,20 @@ generate.sentiment = function(res)
       chartDimensions.sentiment.width = 1550;
       chartDimensions.sentiment.height = 600;
       break;
-
-
-    case 'day' :
-      chartDimensions.class.width = 1550;
-      chartDimensions.class.height = 600;
-      break;
   }
 
-  charts.sentiment.renderTo('#SentimentChart',{width: chartDimensions.sentiment.width, height: chartDimensions.sentiment.height});
+  charts.sentiment.renderTo('#SentimentChart',{width: chartDimensions.sentiment.width, height:chartDimensions.sentiment.height});
+}
 
 
   $(window).resize(function() {
     clearTimeout(window.resizedFinished);
     window.resizedFinished = setTimeout(function(){
-        charts.sentiment.resize({width: chartDimensions.sentiment.width, height: chartDimensions.sentiment.height});
-    }, 250);
+          charts.sentiment.renderTo('#SentimentChart',{width: chartDimensions.sentiment.width, height:chartDimensions.sentiment.height});
+          charts.frequency.renderTo('#FrequencyChart',{width: chartDimensions.sentiment.width, height:chartDimensions.sentiment.height});
+          charts.class.renderTo('#ClassChart',{width: chartDimensions.class.width, height: chartDimensions.class.height});
+        }, 1000);
   });
-
-}
 
 function labelize(str) {
    str = str.split("_").join(" ");
@@ -426,7 +531,11 @@ function toggle_graph_res(res, graph)
 function reset(graph)
 {
   // $("#" + graphType[graph] + "." + graph + "Type").removeClass("active");
-  graphType[graph] = 'stacked-bar';
+  if(graph == 'frequency'){
+    graphType[graph] = 'bar';
+  } else{
+    graphType[graph] = 'stacked-bar';
+  }
   // $("#" + graphType[graph] + "." + graph + "Type").addClass("active");
   $("#" + graph + "TypeButton").html($("#" + graphType[graph] + "." + graph + "Type").html());
   // $("#" + graphRes[graph] + "." + graph + "Res").removeClass("active");
